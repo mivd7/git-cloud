245240ba81049850b4d170fd8b907f30a2ad8857
MIVD <mi.vandeurzen@gmail.com>
2018-06-02 16:28:17 +0200

    first section done

---
+++ homework-week1.js
@@ -0,0 +1,42 @@
+// Variables
+let hero = {
+  name: 'Supermax',
+  heroic: true,
+  inventory: ['a','b'],
+  health: 100
+}
+
+hero.weapon = {
+  type: 'AK-47',
+  damage: 100
+}
+
+console.log(hero)
+
+
+function rest(creature) {
+    console.log(arguments)
+    health: 10
+    return creature
+  }
+
+rest();
+
+
+
+
+//console.log(arguments)
+//health: 10,
+//resting: function rest(creature) {
+  //return creature
+// Game logic
+//
+//Declare the global variable hero and assign it an object. This object has the following keys and values:
+
+//1. `name` which is a string (you choose the name)
+//2. `heroic` which is a boolean
+//3. `inventory` which is an array
+//4. `health` which is a number
+//5. `weapon` which is an object (it has some stats in the form keys and values)
+//6. `weapon` has a key `type` which is a string
+//7. `weapon` has a key `damage` which is a number
